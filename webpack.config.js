dbe449c3348ec619ba72b7f5b102f2bd3691dda5
Daria Makarova <daria.e.makarova@gmail.com>
2018-09-12 13:57:52 +0200

    Use path.resolve in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+const path = require('path');
+
 module.exports = {
     entry: './web/assets/js/RepLogApp.js',
     output: {
-        path: __dirname + '/web/build',
+        path: path.resolve(__dirname, 'web', 'build'),
         filename: 'rep_log.js'
     }
 };
