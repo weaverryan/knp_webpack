0a5a47a65a9b85da819bed6dd751124cbc58bab6
Daria Makarova <daria.e.makarova@gmail.com>
2018-09-12 18:18:43 +0200

    Add @babel/polyfill and include it so that Promise can be used everywhere

--- web/assets/js/layout.js
+++ web/assets/js/layout.js
@@ -3,6 +3,10 @@ const $ = require('jquery');
 require('popper.js');
 require('bootstrap');
 
+// Add babel polyfill on every page to ensure we can
+// use Promise.
+require('@babel/polyfill');
+
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
