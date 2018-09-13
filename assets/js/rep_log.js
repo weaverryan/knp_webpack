d416e8f8cb531fb97681eefdfc0220b95c1f7374
Daria Makarova <daria.e.makarova@gmail.com>
2018-09-13 11:27:09 +0200

    Expose bootstrap decorated jQuery object globally to support legacy code in template

--- assets/js/rep_log.js
+++ assets/js/rep_log.js
@@ -1,6 +1,13 @@
 const $ = require('jquery');
 const RepLogApp = require('babel-loader!./components/RepLogApp');
 
+// Require bootstrap so that popover() is made available.
+require('bootstrap-sass');
+
+// Expose bootstrap decorated jQuery object globally
+// to support legacy code.
+window.$ = $;
+
 $(document).ready(function() {
     var $wrapper = $('.js-rep-log-table');
     var repLogApp = new RepLogApp($wrapper);
